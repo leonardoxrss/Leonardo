dd4e6c8b0a87133c9fa6798e763097db7ba89191
leonardo <leonardoxrss@gmail.com>
2024-07-16 11:19:05 -0300

    leo

---
+++ Leonardo.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+serial.println("adeus memoria");
+}
